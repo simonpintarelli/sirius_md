0de26ddd641dc19c3803b43f83ee3358d37ef446
Edrick Solis Gonzalez <edrick.solisgonzalez@epfl.ch>
2023-07-31 17:45:54 +0200

    fix relative forces & add density tolerance

--- sirius_md/cpmd.py
+++ sirius_md/cpmd.py
@@ -25,8 +25,35 @@ def shake(Cn, C, etol = 5e-15, max_iter = 100 ):
             break
     return Cp, XC
 
+def shake_gamma(Cn, C, etol = 5e-15, max_iter = 100 ):
+    log.debug("shake_gamma!")
+    log.debug(f"{(Cn.H.conjugate())[0,0][:, 0][:, np.newaxis]}")
+    log.debug(f"{(Cn.conjugate())[0,0][0, :][np.newaxis, :] }")
+    log.debug(f"{((Cn.H.conjugate())[0,0][:, 0][:, np.newaxis]*(Cn.conjugate())[0,0][0, :][np.newaxis, :]).shape }")
+    A = 2* (Cn.H @ Cn).real 
+    Arest = identity_like(A)
+    Arest[0,0] = (Cn.H.conjugate())[0,0][:, 0][:, np.newaxis]*(Cn.conjugate())[0,0][0, :][np.newaxis, :]
+    A-= Arest
+    Brest = identity_like(A)
+    Brest[0,0] = (C.H.conjugate())[0,0][:, 0][:, np.newaxis]*(Cn.conjugate())[0,0][0, :][np.newaxis, :] 
+    B = 2* (C.H @ Cn).real - Brest 
+    I = identity_like(A)
+    Xn = 0.5 * (I - A)
+    for i in range(max_iter): #TODO: Add the number of iteration in input file
+        Xn = 0.5*(I - A + Xn @ (I - B) + (I - B.H) @ Xn - (Xn @ Xn))
+        XC = C @ Xn.H
+        Cp = Cn + XC # eq. (4.3) in T&P
+        log.debug(f"{Cp}")
+        error = np.max(np.abs((Cp.H@Cp-I)[0,0])) #TODO: generalize
+        log.debug(f"error shake : {error}")
+        if error < etol:
+            log.debug("Shake finished!")
+            break
+    return Cp, XC
+    
+
 def rattle(un, Cn, XC, dt):
-    #un = un + XC/dt # eq. (4.9) in T&P. Note that XC = (dt^2/2 me)\sum_j\Lambda_{ij}C_j
+    un = un + XC/dt # eq. (4.9) in T&P. Note that XC = (dt^2/2 me)\sum_j\Lambda_{ij}C_j
     D = Cn.H@un # in T&P, C is used instead of D
     Y = -0.5*(D+D.H)
     YC =  Cn @ Y.H
@@ -38,17 +65,6 @@ def rattle(un, Cn, XC, dt):
 
 def update_sirius(sirius_dft_gs: DFT_ground_state):
     kset = sirius_dft_gs.k_point_set()
-    # update density and potential 
-    #use_sym = kset.ctx().use_symmetry()
-    #sirius_dft_gs.density().generate(
-    #    kset,
-    #    use_sym,
-    #    False,  # add core (only for lapw)
-     #   True,  # transform to real space grid
-    #)
-    #sirius_dft_gs.potential().generate(
-    #    sirius_dft_gs.density(), use_sym, True  # transform to real space grid
-        #)
     H = ApplyHamiltonian(sirius_dft_gs.potential(), sirius_dft_gs.k_point_set())
     E = Energy(sirius_dft_gs.k_point_set(), sirius_dft_gs.potential(), sirius_dft_gs.density(), H)
     E.compute(kset.C, kset.fn)
@@ -58,6 +74,9 @@ class CPMDForce:
 
     def __init__(self, sirius_dft_gs: DFT_ground_state):
         self.sirius_dft_gs = sirius_dft_gs
+        self.unit_cell = self.sirius_dft_gs.k_point_set().ctx().unit_cell() 
+        self.L = self.unit_cell.lattice_vectors()
+        self.Lh = np.linalg.inv(self.L)
 
         self.H = ApplyHamiltonian(sirius_dft_gs.potential(), sirius_dft_gs.k_point_set())
         # create object to compute the total energy
@@ -87,7 +106,9 @@ class CPMDForce:
         # Kohn-Sham energy and H*Psi
         Eks, Hx = self.E.compute(C, fn)
 
+
         # Forces acting on nuclei
         F = np.array(self.sirius_dft_gs.forces().calc_forces_total()).T
 
-        return F, Eks, Hx
+
+        return F@self.Lh.T, Eks, Hx
